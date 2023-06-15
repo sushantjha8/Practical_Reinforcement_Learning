0582865baf1603dc7a0143e9321c6b35708ac139
sushantjha8 <sushantjha8@gmail.com>
2023-06-15 18:22:54 +0530

    added model training

--- chapter4/main.py
+++ chapter4/main.py
@@ -8,6 +8,7 @@ import torch
 import numpy as np
 import gym
 from collections import namedtuple
+import time
 
 
 class Net(torch.nn.Module):
@@ -23,16 +24,16 @@ class Net(torch.nn.Module):
         return self.net(x)
 
 
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 Episode = namedtuple("Episode", field_names=["reward", "steps"])
 Episode_steps = namedtuple("Episode_Step", field_names=["obs", "act"])
 # Intial ;ise gym env
-PERCENTILE = 59.0
+PERCENTILE = 73.0
 env = gym.make("CartPole-v1", render_mode="human")
 obs_size = env.observation_space.shape[0]
 label_action = env.action_space.n
 print(f"label {label_action}")
-net = Net(obs_size, hidden_size=4, n_action=label_action)
+net = Net(obs_size, hidden_size=128, n_action=label_action)
 
 
 def iterate_batches(env, net, batch_size=BATCH_SIZE):
@@ -48,11 +49,13 @@ def iterate_batches(env, net, batch_size=BATCH_SIZE):
         action_probility = sm(net(obs_v))
         act_prob = action_probility.data.numpy()[0]
         # select random action from action probalities
-        action = np.random.choice(len(act_prob), p=act_prob)
+        # action = np.random.choice(len(act_prob), p=act_prob)
+        action = act_prob.argmax()
         next_obs, reward, _is_done, _, _ = env.step(action)
-
+        episode_reward += reward
+        step = Episode_steps(obs=obs, act=action)
         if _is_done:
-            e = Episode(reward=reward, steps=step)
+            e = Episode(reward=episode_reward, steps=step)
             batch.append(e)
             episode_step = []
             next_obs = env.reset()
@@ -61,8 +64,7 @@ def iterate_batches(env, net, batch_size=BATCH_SIZE):
             yield batch
             batch = []
             break
-        episode_reward += reward
-        step = Episode_steps(obs=obs, act=action)
+
         # print(next_obs)
         episode_step.append(step)
         obs = next_obs
@@ -71,8 +73,10 @@ def iterate_batches(env, net, batch_size=BATCH_SIZE):
 def filter_batch(batch, pencentile=PERCENTILE):
     _, batch = batch
     rewards = list(map(lambda s: s.reward, batch))
+    # print(f"filter reward {rewards}")
     reward_bound = np.percentile(rewards, pencentile)
     reward_mean = float(np.mean(rewards))
+    # print(f"meean reawrd filter from batch {reward_mean}")
     train_obs = []
     train_act = []
     for reward, step in batch:
@@ -85,11 +89,65 @@ def filter_batch(batch, pencentile=PERCENTILE):
     return train_obs_v, train_act_v, reward_bound, reward_mean
 
 
-itrator = iterate_batches(env, net)
-print(len(list(itrator)))
+objective = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=net.parameters(), lr=0.01)
 
-for batch in enumerate(iterate_batches(env, net)):
-    train_obs_v, train_act_v, reward_bound, reward_mean = filter_batch(batch)
-    print(train_obs_v, train_act_v.size(), reward_bound, reward_mean)
-    obsv = net(train_obs_v)
-    print(obsv)
+count = 1
+
+
+for e in range(0, 20):
+    itrator = iterate_batches(env, net)
+    for batch in enumerate(iterate_batches(env, net)):
+        train_obs_v, train_act_v, reward_bound, reward_mean = filter_batch(batch)
+        optimizer.step()
+        obsv = net(train_obs_v)
+        loss_v = objective(obsv, train_act_v.to(dtype=torch.long))
+        loss_v.backward()
+        l = loss_v.item()
+        optimizer.step()
+        count += count
+        print(f"Action reward,loss {reward_mean, l}")
+        # if loss_v.item() < 0.01:
+        if reward_mean > 100:
+            print(f" Final Epoch {e} loss {loss_v.item(), reward_mean}")
+            break
+    if l < 0.0001:
+        break
+
+
+# torch.save(net, "model/chapter4a")
+# print("----------------Inference------------------------------------------\n")
+# sm = torch.nn.Softmax(dim=1)
+# obs, _ = env.reset()
+# total_reward = 0
+# rl = []
+# rm = 0
+# while True:
+#     obs_v = torch.FloatTensor([obs])
+#     action_probility = sm(net(obs_v))
+#     act_prob = action_probility.data.numpy()[0]
+#     # select random action from action probalities
+#     # action = np.random.choice(len(act_prob), p=act_prob)
+#     action = act_prob.argmax()
+#     print(f" Print Action {action}")
+#     next_obs, reward, _is_done, _, _ = env.step(action)
+#     print(reward)
+#     if reward == 0:
+#         total_reward -= 1
+#     if reward == 1:
+#         total_reward += 1
+#     # # [Cart Position,Cart Velocity,Pole Angle,Pole Angular Velocity] will be our feture from env obsercation
+#     print(
+#         f"Action {action} Reward {reward} Total rerward {total_reward} Action probability ditribution {act_prob}"
+#     )
+#     if _is_done:
+#         # rm = float(np.mean(rl))
+#         # print(f" balaced at Pole Angle {rm}")
+#         obs, _ = env.reset()
+#         # if rm > 100:
+#         #     rl = 0
+#         #     total_reward = 0
+#         total_reward = 0
+
+#     if total_reward > 100:
+#         break
