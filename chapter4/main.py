ac0161e7abc3ceb1879ce3bab75a7670156ed516
sushantjha8 <sushantjha8@gmail.com>
2023-06-16 17:57:35 +0530

    done cartpole training

--- chapter4/main.py
+++ chapter4/main.py
@@ -1,153 +1,175 @@
-""" 
-The objective is train our policy nn Net to give best actions to get max reward to balance game pole.
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+env = gym.make("CartPole-v1", render_mode="human")
+# env.seed(1)
+torch.manual_seed(1)
+
+# Hyperparameters
+learning_rate = 0.01
+gamma = 0.99
+
+# lets build Policy
+# few points for making policy
+"""
+Policy is a Non linear model which stores 
+# Overall reward and loss history
+        self.reward_history = []
+        self.loss_history = []
+        self.reset()
+        self.forrward(x)
 
 """
 
 
-import torch
-import numpy as np
-import gym
-from collections import namedtuple
-import time
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        obs_space_shape = env.observation_space.shape[0]
+        print(f"observation space {obs_space_shape }")
+        action_space = env.action_space.n
+        num_hidden = 128
+        self.l1 = nn.Linear(obs_space_shape, num_hidden, bias=False)
+        self.l2 = nn.Linear(num_hidden, action_space, bias=False)
 
+        # Overall reward and loss history
+        self.reward_history = []
+        self.loss_history = []
+        self.reset()
 
-class Net(torch.nn.Module):
-    def __init__(self, obs_size, hidden_size, n_action):
-        super(Net, self).__init__()
-        self.net = torch.nn.Sequential(
-            torch.nn.Linear(obs_size, hidden_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden_size, n_action),
-        )
+    def reset(self):
+        """rest your episode history and episode_reward history"""
+        # Episode policy and reward history
+        self.episode_actions = torch.Tensor([])
+        self.episode_rewards = []
 
     def forward(self, x):
-        return self.net(x)
+        model = torch.nn.Sequential(
+            self.l1, nn.Dropout(p=0.5), nn.ReLU(), self.l2, nn.Softmax(dim=-1)
+        )
+        return model(x)
 
 
-BATCH_SIZE = 16
-Episode = namedtuple("Episode", field_names=["reward", "steps"])
-Episode_steps = namedtuple("Episode_Step", field_names=["obs", "act"])
-# Intial ;ise gym env
-PERCENTILE = 73.0
-env = gym.make("CartPole-v1", render_mode="human")
-obs_size = env.observation_space.shape[0]
-label_action = env.action_space.n
-print(f"label {label_action}")
-net = Net(obs_size, hidden_size=128, n_action=label_action)
+def predict(obs):
+    obs = torch.FloatTensor(obs)
+    # print(obs.size())
+    action_probs = policy(obs)
+    distribution = Categorical(action_probs)
+    action = distribution.sample()
+    # TODO append episode action to policy
+    policy.episode_actions = torch.cat(
+        [policy.episode_actions, distribution.log_prob(action).reshape(1)]
+    )
+    return action
 
 
-def iterate_batches(env, net, batch_size=BATCH_SIZE):
-    sm = torch.nn.Softmax(dim=1)
-    obs = env.reset()
-    obs, _ = obs
-    batch = []
-    episode_reward = 0.0
-    episode_step = []
-    while True:
-        # [Cart Position,Cart Velocity,Pole Angle,Pole Angular Velocity] will be our feture from env obsercation
-        obs_v = torch.FloatTensor([obs])
-        action_probility = sm(net(obs_v))
-        act_prob = action_probility.data.numpy()[0]
-        # select random action from action probalities
-        # action = np.random.choice(len(act_prob), p=act_prob)
-        action = act_prob.argmax()
-        next_obs, reward, _is_done, _, _ = env.step(action)
-        episode_reward += reward
-        step = Episode_steps(obs=obs, act=action)
-        if _is_done:
-            e = Episode(reward=episode_reward, steps=step)
-            batch.append(e)
-            episode_step = []
-            next_obs = env.reset()
-            next_obs, _ = next_obs
-        if len(batch) == batch_size:
-            yield batch
-            batch = []
+def discount_future_reward(episode_rewards=[], gamma=0.06):
+    rewards = []
+    R = 0
+    # Discount future rewards back to the present using gamma
+    for r in episode_rewards[::-1]:
+        R = r + gamma * R
+        rewards.insert(0, R)
+
+    # Scale rewards
+    rewards = torch.FloatTensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+
+    return rewards
+
+
+def update_policy():
+    episode_rewards = policy.episode_rewards
+    rewards = discount_future_reward(episode_rewards=episode_rewards, gamma=gamma)
+    # Calculate loss
+    loss = torch.sum(torch.mul(policy.episode_actions, rewards).mul(-1), -1)
+
+    # Update network weights
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Save and intialize episode history counters
+    policy.loss_history.append(loss.item())
+    policy.reward_history.append(np.sum(policy.episode_rewards))
+    policy.reset()
+
+
+def train(episodes):
+    list_score = []
+    print(env.spec.reward_threshold)
+    for episode in range(episodes):
+        # TODO: rest Env
+        obs, _ = env.reset()
+
+        obs = torch.from_numpy(obs)
+        for step in range(0, 1000):
+            # TODO  get action from policy prediction
+            action = predict(obs)
+            env.render()
+            # print(action)
+
+            # TODO get next_obs , reward by applyinction action.item()
+            next_obs, reward, _is_done, _, _ = env.step(action.item())
+            # Save reward
+            policy.episode_rewards.append(reward)
+            obs = next_obs
+            if _is_done:
+                break
+        # TODO update policy
+        update_policy()
+
+        list_score.append(step)
+        mean_score = np.mean(list_score[-100:])
+
+        if episode % 50 == 0:
+            print(
+                "Episode {}\tAverage length (last 100 episodes): {:.2f} time steps.{}".format(
+                    episode, mean_score, step
+                )
+            )
+
+        if mean_score > env.spec.reward_threshold:
+            print(
+                "Solved after {} episodes! Running average is now {}. Last episode ran to {} time steps.".format(
+                    episode, mean_score, step
+                )
+            )
             break
 
-        # print(next_obs)
-        episode_step.append(step)
-        obs = next_obs
 
+policy = Policy()
 
-def filter_batch(batch, pencentile=PERCENTILE):
-    _, batch = batch
-    rewards = list(map(lambda s: s.reward, batch))
-    # print(f"filter reward {rewards}")
-    reward_bound = np.percentile(rewards, pencentile)
-    reward_mean = float(np.mean(rewards))
-    # print(f"meean reawrd filter from batch {reward_mean}")
-    train_obs = []
-    train_act = []
-    for reward, step in batch:
-        if reward < reward_bound:
-            continue
-        train_obs.append(step.obs)
-        train_act.append(step.act)
-    train_obs_v = torch.FloatTensor(train_obs)
-    train_act_v = torch.FloatTensor(train_act)
-    return train_obs_v, train_act_v, reward_bound, reward_mean
+optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
+train(episodes=1000)
+window = 50
 
+fig, ((ax1), (ax2)) = plt.subplots(2, 1, sharey=True, figsize=[9, 9])
+rolling_mean = pd.Series(policy.reward_history).rolling(window).mean()
+std = pd.Series(policy.reward_history).rolling(window).std()
+ax1.plot(rolling_mean)
+ax1.fill_between(
+    range(len(policy.reward_history)),
+    rolling_mean - std,
+    rolling_mean + std,
+    color="orange",
+    alpha=0.2,
+)
+ax1.set_title("Episode Length Moving Average ({}-episode window)".format(window))
+ax1.set_xlabel("Episode")
+ax1.set_ylabel("Episode Length")
 
-objective = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(params=net.parameters(), lr=0.01)
+ax2.plot(policy.reward_history)
+ax2.set_title("Episode Length")
+ax2.set_xlabel("Episode")
+ax2.set_ylabel("Episode Length")
 
-count = 1
-
-
-for e in range(0, 50):
-    itrator = iterate_batches(env, net)
-    for batch in enumerate(iterate_batches(env, net)):
-        train_obs_v, train_act_v, reward_bound, reward_mean = filter_batch(batch)
-        optimizer.step()
-        obsv = net(train_obs_v)
-        loss_v = objective(obsv, train_act_v.to(dtype=torch.long))
-        loss_v.backward()
-        l = loss_v.item()
-        optimizer.step()
-        count += count
-        print(f"Action reward,loss {reward_mean, l}")
-        # if loss_v.item() < 0.01:
-        if reward_mean > 100:
-            print(f" Final Epoch {e} loss {loss_v.item(), reward_mean}")
-            break
-    if l < 0.0001:
-        break
-
-
-# torch.save(net, "model/chapter4a")
-# print("----------------Inference------------------------------------------\n")
-# sm = torch.nn.Softmax(dim=1)
-# obs, _ = env.reset()
-# total_reward = 0
-# rl = []
-# rm = 0
-# while True:
-#     obs_v = torch.FloatTensor([obs])
-#     action_probility = sm(net(obs_v))
-#     act_prob = action_probility.data.numpy()[0]
-#     # select random action from action probalities
-#     # action = np.random.choice(len(act_prob), p=act_prob)
-#     action = act_prob.argmax()
-#     print(f" Print Action {action}")
-#     next_obs, reward, _is_done, _, _ = env.step(action)
-#     print(reward)
-#     if reward == 0:
-#         total_reward -= 1
-#     if reward == 1:
-#         total_reward += 1
-#     # # [Cart Position,Cart Velocity,Pole Angle,Pole Angular Velocity] will be our feture from env obsercation
-#     print(
-#         f"Action {action} Reward {reward} Total rerward {total_reward} Action probability ditribution {act_prob}"
-#     )
-#     if _is_done:
-#         # rm = float(np.mean(rl))
-#         # print(f" balaced at Pole Angle {rm}")
-#         obs, _ = env.reset()
-#         # if rm > 100:
-#         #     rl = 0
-#         #     total_reward = 0
-#         total_reward = 0
-
-#     if total_reward > 100:
-#         break
+fig.tight_layout(pad=2)
+plt.show()
